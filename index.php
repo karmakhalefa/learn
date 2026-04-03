e4fb5f5bb2f8460d80013316cf2cf667aca7f155
karma khalefa <karrma.khalefa@gmail.com>
2026-04-03 16:10:10 +0200

    first

--- index.php
+++ index.php
@@ -104,3 +104,22 @@ echo "</pre>";
 
 
 ?>
+<html>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        
+    <form method="get">
+
+    <input  type="text" name="username" placeholder="Username" class="username">name </input>
+      <input  type="number" name="password" placeholder="Password" class="password">password</input>
+        <button type="submit" class="login">save</button>
+</form>
+    </body>
+    </html>
+</html>
