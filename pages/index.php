c5d335e50a772bd50da9e632ffd499395783e642
karma khalefa <karrma.khalefa@gmail.com>
2026-04-05 14:58:49 +0200

    create login page

--- pages/index.php
+++ pages/index.php
@@ -104,30 +104,3 @@ echo "</pre>";
 
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="css/style.css">
-    <title>Document</title>
-</head>
-<body>
-<form method="get">
-    <div class="form_name"> 
-
-        <label class="label_Name">Name</label>
-    <input type="text" name="username" placeholder="Username" class="username">
-
-</div>
-     <div class="form_password"> 
-
-       <label class="label_password">Password</label>
-    <input type="number" name="password" placeholder="Password" class="password">
-
-    </div>
-
-    <input type="submit" class="login" value="Register"></input>
-</form> 
-</body>
-</html>
---
+++ pages/login.php
@@ -0,0 +1,52 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="../asstes/style.css">
+    <title>Document</title>
+</head>
+<body class="body_login">
+    <div class="login_page">
+    <div class="headr_form">
+    <h1> Zync System</h1>
+    <h2>Sign In</h2>
+    <p>Enter your credentials to access your account</p>
+    </div>
+    
+<form method="get">
+       
+    <div class="form_name"> 
+
+        <label class="label_Name"> Name</label>
+    <input type="text" name="username" placeholder="Enter your name" class="username">
+
+</div>
+ <div class="form_email"> 
+
+<label class="label_email">email</label>
+<input type="text" name="email" placeholder="Enter your email" class="input_email">
+
+</div>
+
+     <div class="form_password"> 
+
+       <label class="label_password">Password</label>
+    <input type="number" name="password" placeholder="Enter your password" class="password">
+        </div>
+    <div class="confirm_Password">  
+         <label class="label_confirm-password"> confirm Password</label>
+    <input type="number" name="password" placeholder="Enter your confirm password" class="input_confirm_Password">
+</div>
+
+
+
+    <input type="submit" class="login" value="login"></input>
+    <p>Forgot your password?  
+        <span>Reset Password</span>
+    </p>
+</form> 
+</div>
+</body>
+</html>
